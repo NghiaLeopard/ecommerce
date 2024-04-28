b27aab0d71b09e5fb2f1b46fe9377d561b4a9803
DaiNghiadz~ <nghiabeo1605gmail.com0>
2024-04-28 08:55:02 +0700

    dark mode - multiple language

--- src/configs/i18n.ts
+++ src/configs/i18n.ts
@@ -1,30 +1,52 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import LanguageDetector from 'i18next-browser-languagedetector'
+import Backend from 'i18next-http-backend'
 
 i18n
+  .use(Backend)
 
   // Enable automatic language detection
   .use(LanguageDetector)
 
   // Enables the hook initialization module
   .use(initReactI18next)
+
+  // initialize
   .init({
-    lng: 'en',
+    lng: 'vi',
     backend: {
       /* translation file path */
       loadPath: '/locales/{{lng}}.json'
     },
-    fallbackLng: 'en',
+
+    // default
+    fallbackLng: 'vi',
     debug: false,
     keySeparator: false,
     react: {
+      //
       useSuspense: false
     },
     interpolation: {
+      // escapeValue: false then it will understand contain html ,...
+      // escapeValue: true then it understand to key
       escapeValue: false,
+
+      // format 100000: 100,000
       formatSeparator: ','
     }
   })
 
 export default i18n
+
+export const LANGUAGE_OPTION = [
+  {
+    lang: 'TIếng Việt',
+    value: 'vi'
+  },
+  {
+    lang: 'English',
+    value: 'en'
+  }
+]
---
+++ src/view/layout/components/language-dropdown/index.tsx
@@ -0,0 +1,90 @@
+// ** Next
+import { NextPage } from 'next'
+
+// ** React
+import React, { MouseEvent, useState } from 'react'
+
+// ** Mui
+import { IconButton, Menu, MenuItem, Typography } from '@mui/material'
+
+// ** Components
+import CustomIcon from 'src/components/Icon'
+
+// ** i18n
+import { useTranslation } from 'react-i18next'
+import { LANGUAGE_OPTION } from 'src/configs/i18n'
+
+interface TProps {}
+
+const LanguageDropDown: NextPage<TProps> = () => {
+  // ** State
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+
+  // ** Hook
+  const { i18n } = useTranslation()
+
+  const open = Boolean(anchorEl)
+  const handleClick = (event: MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget)
+  }
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
+  const handleChangeLanguage = (lang: string) => {
+    i18n.changeLanguage(lang)
+  }
+
+  return (
+    <div>
+      <IconButton color='inherit' onClick={handleClick}>
+        <CustomIcon icon='fa:language' />
+      </IconButton>
+      <Menu
+        anchorEl={anchorEl}
+        id='account-menu'
+        open={open}
+        onClose={handleClose}
+        onClick={handleClose}
+        PaperProps={{
+          elevation: 0,
+          sx: {
+            overflow: 'visible',
+            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+            mt: 1.5,
+            '& .MuiAvatar-root': {
+              width: 32,
+              height: 32,
+              ml: -0.5,
+              mr: 1
+            },
+            '&::before': {
+              content: '""',
+              display: 'block',
+              position: 'absolute',
+              top: 0,
+              right: 14,
+              width: 10,
+              height: 10,
+              bgcolor: 'background.paper',
+              transform: 'translateY(-50%) rotate(45deg)',
+              zIndex: 0
+            }
+          }
+        }}
+        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+      >
+        {LANGUAGE_OPTION.map(item => {
+          return (
+            <MenuItem key={item.value} onClick={() => handleChangeLanguage(item.value)}>
+              <Typography>{item.lang}</Typography>
+            </MenuItem>
+          )
+        })}
+      </Menu>
+    </div>
+  )
+}
+
+export default LanguageDropDown
