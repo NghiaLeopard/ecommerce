9005beccf6485c46c1011030eb00e758eac38c03
DaiNghiadz~ <nghiabeo1605gmail.com0>
2024-06-17 08:39:27 +0700

    fix comment eslint

--- src/pages/api/auth/[...nextauth]/index.ts
+++ src/pages/api/auth/[...nextauth]/index.ts
@@ -10,14 +10,12 @@ export const authOptions = {
   ],
   callbacks: {
     async jwt({ token, account }: any) {
-      // Persist the OAuth access_token to the token right after signin
       if (account) {
         token.accessToken = account.access_token
       }
       return token
     },
     async session({ session, token, user }: any) {
-      // Send properties to the client, like an access_token from a provider.
       session.accessToken = token.accessToken
       return session
     }
